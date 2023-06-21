05ac3e0c2367f2ab0bb7458d65e4880790d95aea
MANASA TONDUR <102951222+CodeWith-Manasa@users.noreply.github.com>
2023-06-21 09:49:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import Home from './pages/home';
+import Cart from './pages/cart';
+import Navbar from './components/navbar';
+
+function App() {
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/cart" element={<Cart />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
+}
+
+export default App;
